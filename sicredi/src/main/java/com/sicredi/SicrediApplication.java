7b78357148678937d3d91ed31c422db2c6a34df3
felippe_alacoque <felippe.alacoque@ggbank.tech>
2022-12-24 10:59:06 -0300

    Commit inicial + conexão banco MySQL

---
+++ sicredi/src/main/java/com/sicredi/SicrediApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediApplication.class, args);
+	}
+
+}
